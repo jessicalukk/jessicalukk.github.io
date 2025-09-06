e93a2ad5646420737c16dff5a2bb32b953a3e33a
jessicalukk <jessicalukk@gmail.com>
2025-09-05 20:01:36 -0400

    fix: stable Pages config + mobile-safe social dock

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './styles/global.css'
+import './styles/global.css' // adjust path if your stylesheet lives elsewhere
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
